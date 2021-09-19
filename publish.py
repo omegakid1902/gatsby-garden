13006c79f5ece696c315d3478bdf363223354108
omegakid1902 <anhdungbk09@gmail.com>
2021-09-19 09:40:13 +0700

    Update publish.py

--- publish.py
+++ publish.py
@@ -6,18 +6,18 @@ import os
 if not os.path.exists('./content'):
     os.mkdir('./content')
 
-for file in os.listdir("./zettelkasten/"):
+for file in os.listdir("../zettelkasten/"):
     if file.endswith(".md"):
-        with open(os.path.join("./zettelkasten/", file), encoding="utf8") as f:
+        with open(os.path.join("../zettelkasten/", file), encoding="utf8") as f:
             content = f.read()
             metadata, content = frontmatter.parse(content)
             if 'publish' in metadata.keys():
                 print("Copy publish files from zettelkasten to content/")
-                copy(os.path.join("./zettelkasten/", file), './content/')
+                copy(os.path.join("../zettelkasten/", file), './content/')
             else:
                 pass
 
-for root, dirs, files in os.walk("./zettelkasten/Zet/"):
+for root, dirs, files in os.walk("../zettelkasten/Zet/"):
     for file in files:
         if file.endswith(".md"):
             with open(os.path.join(root, file), encoding="utf8") as f:
